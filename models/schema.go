897ce0a30e57acd0b9520dd00d985b183759f152
莲示 <lianshi.whb@alibaba-inc.com>
2018-11-27 15:15:09 +0800

    use soft delete

--- models/schema.go
+++ models/schema.go
@@ -1,7 +1,11 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 type Person struct {
-	ID    uint   `json:"id"`
+	gorm.Model
 	Name  string `json:"name" binding:"required,max=128"`
 	Age   uint8  `json:"age" binding:"required,gte=1,lte=130"`
 	Email string `json:"email" binding:"required,email"`
